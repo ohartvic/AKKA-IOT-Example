a6d8331750b93f89f8395bb1672de106cfe4c7cb
ottohartvich <otto.hartvich@cz.ibm.com@otto-air.praha.cz.ibm.com>
2020-07-22 15:25:34 +0200

    merging

--- src/test/java/com/lightbend/akka/sample/DeviceTest.java
+++ src/test/java/com/lightbend/akka/sample/DeviceTest.java
@@ -1,6 +1,8 @@
 package com.lightbend.akka.sample;
 
-import java.time.Duration;
+import static org.junit.Assert.assertEquals;
+<<<<<<< HEAD
+
 =======
 import static org.junit.Assert.assertNotEquals;
 
